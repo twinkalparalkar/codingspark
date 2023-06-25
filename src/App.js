2279438875edee7e05a1699329be01a42e7f9a8a
twinkalp <twinkalparalkar2@gmail.com>
2023-06-25 13:24:00 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,47 @@
+
+import './App.css';
+import Expense from './component/Expense';
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p>
+        <div>
+        <h2>Let</h2>
+        <Expense></Expense>
+        </div>
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+        
+      </header>
+    </div>
+    
+  );
+}
+
+export default App;
+
+
+// <div className="App">
+//       <header className="App-header">
+//         <img src={logo} className="App-logo" alt="logo" />
+//         <p>
+//           Edit <code>src/App.js</code> and save to reload.
+//         </p>
+//         <a
+//           className="App-link"
+//           href="https://reactjs.org"
+//           target="_blank"
+//           rel="noopener noreferrer"
+//         >
+//           Learn React
+//         </a>
+//       </header>
+//     </div>
---
+++ src/component/Expense.js
@@ -0,0 +1,16 @@
+function Expense(){
+    return (
+        <div>
+        <h2>Expense Item</h2>
+            <ul>
+                <li>Food Rs 10</li>
+                
+                <li> Petrol Rs 100</li>
+                
+                <li> Movies Rs 200</li>
+            </ul>
+        </div>
+        
+    )
+}
+export default Expense;
