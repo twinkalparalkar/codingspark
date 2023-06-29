9a3097d36808616466e507153b44ed439f0e8eb0
twinkalp <twinkalparalkar2@gmail.com>
2023-06-29 15:41:01 +0530

    Initial6 commit

--- src/component/Expensedetail.js
+++ src/component/Expensedetail.js
@@ -1,18 +1,18 @@
 import {useState} from 'react'
 
 let Expensedetail=(props)=>{
-    const [title,setTitle]=useState(props.title)
+    const [amount,setTitle]=useState(props.amount)
     const item1="Food"
     // let title=props.title
     const clickHandler=()=>{
-        setTitle("Updated")
+        setTitle(100)
         console.log('clicked')
     }
 
     return (
 
        <div>
-       Amount:{props.amount} --title:{title}
+       Amount:{amount} --title:{props.title}
        <button onClick={clickHandler} >Change Title</button>
        </div>
         
