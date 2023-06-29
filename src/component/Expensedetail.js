fb7d1f1b24b0cc1c72b320bfd38c26f27b522c48
twinkalp <twinkalparalkar2@gmail.com>
2023-06-29 15:11:14 +0530

    Initial5 commit

--- src/component/Expensedetail.js
+++ src/component/Expensedetail.js
@@ -1,9 +1,12 @@
 let Expensedetail=(props)=>{
     const item1="Food"
+    const clickHandler=()=>console.log('clicked')
+
     return (
 
        <div>
        Amount:{props.amount} --title:{props.title}
+       <button onClick={clickHandler} >Change Title</button>
        </div>
         
         )
