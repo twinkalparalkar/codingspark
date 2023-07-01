73d50a5005e0d07ee91798c4fbd424c9403d0239
twinkalp <twinkalparalkar2@gmail.com>
2023-07-01 15:36:15 +0530

    Initial14 commit

--- src/App.js
+++ src/App.js
@@ -4,9 +4,9 @@ import Expense from './component/Expense';
 import { useState } from 'react';
 import NewExpense from './component/NewExpense';
 const ex=[
-  {id:1,title:"book1",amount:600,date:"2023-02-03"},
-  {id:2,title:"book2",amount:601,date:"2023-02-03"},
-  {id:3,title:"book3",amount:700,date:"2023-02-03"}
+  {id:1,title:"book1",amount:600,date:new Date("2020-02-03")},
+  {id:2,title:"book2",amount:601,date:new Date("2020-02-03")},
+  {id:3,title:"book3",amount:700,date:new Date("2022-02-03")}
 ]
 let App=() =>{
   const [expense1,setexpense]=useState(ex)
@@ -18,14 +18,6 @@ let App=() =>{
   }
   console.log(expense1)
 
-  // const saveformdataHandler=(enterexpensedata)=>{
-  //   const expenseData={
-  //     ...enterexpensedata,
-  //     id:Math.random().toString()
-  //   }
-  //   console.log(expenseData)
-    
-  // }
   return (
     <div className="App">
         <NewExpense onAddExpense={addExpenseHandler}/>
@@ -38,23 +30,3 @@ let App=() =>{
 
 export default App;
 
-  // <ExpenseForm onSaveForm={saveformdataHandler}/>
-// <Expense title={ex[0].title} amount={ex[0].amount}></Expense>
-        // <Expense title={ex[1].title} amount={ex[1].amount}></Expense>
-        // <Expense title={ex[2].title} amount={ex[2].amount}></Expense>
-// <div className="App">
-//       <header className="App-header">
-//         <img src={logo} className="App-logo" alt="logo" />
-//         <p>
-//           Edit <code>src/App.js</code> and save to reload.
-//         </p>
-//         <a
-//           className="App-link"
-//           href="https://reactjs.org"
-//           target="_blank"
-//           rel="noopener noreferrer"
-//         >
-//           Learn React
-//         </a>
-//       </header>
-//     </div>
--- src/component/Expense.js
+++ src/component/Expense.js
@@ -9,21 +9,31 @@ let Expense=(props)=>{
     if (!props.item || props.item.length === 0) {
         return null; // or handle the empty case appropriately
     }
-    console.log(props.item[0])
+    // console.log(props.item)
 
     const [filterdYear,setFilterYear]=useState('2020')
     const filteredHandler=(selected)=>{
         setFilterYear(selected)
     }
+    console.log("kk",filterdYear)
     return (
         <div className="card1">
             <ExpenseFilter selected={filterdYear} onChangeFilter={filteredHandler}/>
-            {props.item.map(expense=><Expensedetail
-                key={expense.id}
-                title={expense.title}  amount={expense.amount}  date={expense.date}/>)}
 
+            {props.item.filter(expense =>{
+                if(expense.date.getFullYear() =="2020"){
+                    return expense
+                }
+            }).map(expense => (
+                <Expensedetail
+                    key={expense.id}
+                    title={expense.title}
+                    amount={expense.amount}
+                    date={expense.date.toString()}
+                />))}
             
         </div>
     )
 }
 export default Expense;
+
