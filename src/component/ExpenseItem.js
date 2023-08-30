f5d372bbace30a987c3240b3440c592b818ba4dc
twinkalp <twinkalparalkar2@gmail.com>
2023-08-30 12:05:32 +0530

    Initial commit

---
+++ src/component/ExpenseItem.js
@@ -0,0 +1,15 @@
+function ExpenseItem(){
+    return (
+        <div>
+        <h1>Expense Items !!</h1>
+        <div><h2>title</h2></div>
+        <div><h2>Amount</h2></div>
+        <div><h2>Food</h2></div>
+        <div><h2>Rs 120</h2></div>
+        <div><h2>Petrol</h2></div>
+        <div><h2>Rs 200</h2></div>
+        </div>
+    )
+    
+}
+export default ExpenseItem;
