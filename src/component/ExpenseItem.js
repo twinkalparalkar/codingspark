942644fe0d40e13ca0479bb86cf51302d0f9925b
twinkalp <twinkalparalkar2@gmail.com>
2023-08-30 13:10:09 +0530

    Initial commit

--- src/component/ExpenseItem.js
+++ src/component/ExpenseItem.js
@@ -1,16 +1,19 @@
 import './ExpenseItem.css'
 
 function ExpenseItem() {
+    const expenseDate=new Date(2021,7,7)
+    const expenseAmount=120
+    const expensetitle="Food"
     return (
         <div className="expense-item">
             <div className="expense-date">
-                <h4>March 6 2021</h4>
+                <h4>{expenseDate.toISOString()}</h4>
             </div>
             <div className="expense-name">
-                <h2>Food</h2>
+                <h2> {expensetitle}</h2>
             </div>
             <div className="expense-amount">
-                <h2>Rs 120</h2>
+                <h2>Rs.{expenseAmount} </h2>
             </div>
 
         </div>
