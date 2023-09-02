6940b6d50582d0bf157f3d1d0678e56299d09681
twinkalp <twinkalparalkar2@gmail.com>
2023-09-02 21:13:57 +0530

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,20 +1,37 @@
 // import logo from './logo.svg';
 // import './App.css';
 
-import Expenses from "./component/Expenses";
+import Expenses from "./component/Expenses/Expenses";
 function App() {
-    const expenses=[
-        {id:'e1',title:"Food",amount:200,date:new Date(2021,7,7)},
-        {id:'e2',title:"movie",amount:400,date:new Date(2021,7,7)},
-        {id:'e3',title:"travelling",amount:800,date:new Date(2021,7,7)},
-        {id:'e4',title:"home",amount:300,date:new Date(2021,7,7)}
+    const expenses = [
+        {
+            id: 'e1',
+            title: "Food",
+            amount: 200,
+            date: new Date(2021, 7, 7)
+        }, {
+            id: 'e2',
+            title: "movie",
+            amount: 400,
+            date: new Date(2021, 7, 7)
+        }, {
+            id: 'e3',
+            title: "travelling",
+            amount: 800,
+            date: new Date(2021, 7, 7)
+        }, {
+            id: 'e4',
+            title: "home",
+            amount: 300,
+            date: new Date(2021, 7, 7)
+        }
     ]
     return (
         <div className="App">
             <header className="App-header">
                 <h1>Expense Items !!</h1>
                 <Expenses items={expenses}/>
-             
+
             </header>
         </div>
     );
