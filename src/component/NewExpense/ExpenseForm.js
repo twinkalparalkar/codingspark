1d19cc284f7b137de9c3bbb83ab6a51a818c267a
twinkalp <twinkalparalkar2@gmail.com>
2023-09-13 12:17:49 +0530

    Initial commit

--- src/component/NewExpense/ExpenseForm.js
+++ src/component/NewExpense/ExpenseForm.js
@@ -1,21 +1,35 @@
 import Card from '../UI/Card';
 import './ExpenseForm.css'
-function ExpenseForm(props) {
-    const changehandler = (e) => {
-        console.log(e.target.value)
-    }
+import React, {useState} from 'react'
 
+function ExpenseForm(props) {
+    const [enteredtitle, setTitle] = useState('')
+    const [enteredAmount, setAmount] = useState('')
+    const [enteredDate, setDate] = useState('')
+
+    const TitleChangehandler = (e) => {
+        setTitle(e.target.value)
+    }
+    const AmountChangehandler = (e) => {
+        setAmount(e.target.value)
+    }
+    const DateChangehandler = (e) => {
+        setDate(e.target.value)
+    }
+    
     return (
         <Card>
             <form className='form'>
                 <label>Expense Title:</label>
                 <input type="text"
-                    onChange={changehandler}/><br/>
+                    onChange={TitleChangehandler}/><br/>
                 <label>Expense Amount:</label>
-                <input type="number"/><br/>
+                <input type="number"
+                    onChange={AmountChangehandler}/><br/>
                 <label>Expense Date:</label>
-                <input type="date" min="2023-01-01" max="2030-12-31"/><br/>
-                
+                <input type="date" min="2023-01-01" max="2030-12-31"
+                    onChange={DateChangehandler}/><br/>
+
                 <input type="submit" className='submit'/>
             </form>
         </Card>
