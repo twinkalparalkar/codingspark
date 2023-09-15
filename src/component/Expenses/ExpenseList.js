5d478e623fa5d8e834c8046ac96e7ba8a201fa41
twinkalp <twinkalparalkar2@gmail.com>
2023-09-15 12:47:18 +0530

    Initial commit

--- src/component/Expenses/ExpenseList.js
+++ src/component/Expenses/ExpenseList.js
@@ -5,8 +5,14 @@ function ExpenseList(props){
     if (props.item.length === 0) {
         return <h3>No expense Found</h3>;
     }
+    let oneElement=<p></p>
+    if(props.item.length === 1){
+     oneElement=<p>Only single Expense here. Please add more...</p>
+    }
+    
      return (
         <ul className="ul">
+        {oneElement}
         {props.item.map((ex) => (
             <ExpenseItem key={
                     ex.id
