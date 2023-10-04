1b871bb8bb1fd8e0c19f60e4eb5b28d3ce294646
twinkalp <twinkalparalkar2@gmail.com>
2023-10-04 11:24:13 +0530

    Initial commit1

--- src/component/Layout/Home.js
+++ src/component/Layout/Home.js
@@ -1,10 +1,30 @@
 import { Container,Row,Col,Button } from "react-bootstrap"
+import {useState }from 'react'
 function Home(){
+    const [movie,setMovie]=useState([])
+    function MovieHandler(){
+        fetch("https://swapi.dev/api/films/").then(res=>{
+            return res.json()
+        }).then(data=>{
+            console.log(data)
+            const transformeddata=data.results.map(movie=>{
+                return {id:movie.episode_id,
+                title:movie.title,
+                producer:movie.producer,
+                release_date:movie.release_date
+            }
+            })
+            console.log(transformeddata)
+            setMovie(transformeddata)
+        })
+    }
+
     return (
 <div>
     <div style={{backgroundColor:"gray",marginTop:"-15px",height:"180px",width:"100%",color:"white",
     fontSize:"30px",alignItems:"center"}}>
-        <p style={{border:"solid 2px #56CCF2",width:"300px",marginLeft:"480px"}}>Get our Latest Album</p>
+        <p style={{border:"solid 2px #56CCF2",width:"300px",marginLeft:"480px"}}
+        onClick={MovieHandler}>Get our Latest Movie</p>
         <div style={{border:"solid 2px #56CCF2",borderRadius:"100%",width:"80px",height:"80px",marginLeft:"580px"}}>
         <div style={{border:"0",
         background: "transparent",
@@ -18,27 +38,18 @@ function Home(){
         borderWidth: "17px 0 17px 30px",margin:"20px 0px 0px 23px"}}>
         </div>
         </div>
+       
     </div>
-    <p style={{fontFamily:"Cursive",fontSize:"30px", padding:"40px"}}><b>TOURS</b></p>
+    <p style={{fontFamily:"Cursive",fontSize:"30px", padding:"40px"}}><b>MOVIES</b></p>
     <Container style={{width:"75%"}}>
-        <Row>
-            <Col>JUL16</Col>
-            <Col>DETROIT, MI</Col>
-            <Col>DTE ENERGY MUSIC THEATRE</Col>
-            <Col><Button>BUY TICKETS</Button></Col>
-        </Row><hr/>
-        <Row>
-            <Col>JUL19</Col>
-            <Col>TORONTO,ON</Col>
-            <Col>BUDWEISER STAGE</Col>
-            <Col><Button>BUY TICKETS</Button></Col>
-        </Row><hr/>
-        <Row>
-            <Col>JUL 22</Col>
-            <Col>BRISTOW, VA</Col>
-            <Col>JIGGY LUBE LIVE</Col>
-            <Col><Button>BUY TICKET</Button></Col>
-        </Row><hr/>
+        {movie.map((m)=>(
+        <Row key={m.id}>
+            <Col>{m.release_date}</Col>
+            <Col>{m.title}</Col>
+            <Col>{m.producer}</Col>
+            <Col><Button>BUY TICKET</Button></Col><hr/>
+        </Row>
+        ))}
     </Container>
 </div>
     )
