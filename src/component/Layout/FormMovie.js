cc2920a769edfd63bc66139714f5d31904e07fb7
twinkalp <twinkalparalkar2@gmail.com>
2023-10-07 19:33:21 +0530

    Initial commit1

--- src/component/Layout/FormMovie.js
+++ src/component/Layout/FormMovie.js
@@ -1,14 +1,45 @@
-function FormMovie(){
+import {useState} from 'react'
+function FormMovie(props){
+    const [title,setTitle]=useState('')
+    const ChangeTitle=(e)=>{
+        setTitle(e.target.value)
+    }
+    const [producer,setproducer]=useState('')
+    const Changeproducer=(e)=>{
+        setproducer(e.target.value)
+    }
+    const [release_date,setrelease_date]=useState('')
+    const Changerelease_date=(e)=>{
+        setrelease_date(e.target.value)
+    }
+    // console.log(title,release_date)
+
+    const OnSubmitHandler=(e)=>{
+        e.preventDefault()
+
+        const movie={
+            id:Math.random(),
+            title:title,
+            producer:producer,
+            release_date:release_date
+        }
+        setTitle("")
+        setproducer("")
+        setrelease_date("")
+        
+        props.onAddData(movie)
+
+    }
     return (
 <div style={{width:"200px",height:"250px",backgroundColor:"#56CCF2",alignItems:"center",marginLeft:"500px"}}>
     <form><br/><b>
         <label>Title : </label>
-        <input type="text"/><br/>
+        <input type="text" value={title} onChange={ChangeTitle}/><br/>
         <label>producer : </label>
-        <input type="text"/><br/>
+        <input type="text" value={producer} onChange={Changeproducer}/><br/>
         <label>release_date : </label>
-        <input type="date"/><br/><br/></b>
-        <input type="submit" value="Add Movie"/>
+        <input type="date" value={release_date} onChange={Changerelease_date}/><br/><br/></b>
+        <input type="submit" value="Add Movie" onClick={OnSubmitHandler}/>
     </form>
 </div>
     )
--- src/component/Layout/Home.js
+++ src/component/Layout/Home.js
@@ -14,17 +14,24 @@ function Home(){
         setLoading(true)
         setError(null)
         try{
-            const res=await fetch("https://swapi.dev/api/films/")
+            const res=await fetch("https://react-http-43b82-default-rtdb.firebaseio.com/movies.json")
             if(!res.ok){throw new Error("Something went wrong ....Retrying")}
             const data=await res.json()
-            const transformeddata=data.results.map(movie=>{
-                    return {id:movie.episode_id,
-                title:movie.title,
-                producer:movie.producer,
-                release_date:movie.release_date
+
+            const loadedMovie=[]
+
+            for(const key in data){
+                loadedMovie.push({
+                    id:key,
+                    title:data[key].title,
+                    producer:data[key].producer,
+                    release_date:data[key].release_date
+                }
+                    
+                )
             }
-            })
-        setMovie(transformeddata)   
+            
+        setMovie(loadedMovie)   
         setLoading(false)
         }
         catch(error){
@@ -33,7 +40,31 @@ function Home(){
             setError(m)
             setLoading(false)
         }
+    }
+
+    async function onAddDataHandler(movie){
+        console.log("kk",movie)
+        const response= await fetch("https://react-http-43b82-default-rtdb.firebaseio.com/movies.json",
+            {method:"POST",
+            body:JSON.stringify(movie),
+            headers:{
+                'Content-Type':'application/json'
+            }
+            });
+        const data=await response.json()
+        console.log(data)
+        MovieHandler();
+    }
+
+    async function onDeleteHandler(e){
+        e.preventDefault()
+        console.log("delete",e.target.id)
+        const response=await fetch(`https://react-http-43b82-default-rtdb.firebaseio.com/movies/${e.target.id}.json`,
+            {method:"DELETE"
+            });
        
+        console.log(response)
+        MovieHandler();
     }
     return (
 <div>
@@ -49,7 +80,7 @@ function Home(){
         </div>
     </div>
     <br/>
-    <FormMovie/>
+    <FormMovie onAddData={onAddDataHandler}/>
     <p style={{fontFamily:"Cursive",fontSize:"30px", padding:"40px"}}><b>MOVIES</b></p>
     <Container style={{width:"75%"}}>
         {error}
@@ -58,6 +89,7 @@ function Home(){
             <Col>{m.release_date}</Col>
             <Col>{m.title}</Col>
             <Col>{m.producer}</Col>
+            <Col><Button variant="danger"id={m.id}  onClick={onDeleteHandler}>Delete</Button></Col>
             <Col><Button>BUY TICKET</Button></Col><hr/>
         </Row>
         ))}
